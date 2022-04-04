ead4cc083dd70d8010ea6f51372faff55e5af6ce
Joel <91497556+jo9434@users.noreply.github.com>
2022-04-05 00:23:34 +0530

    Add files via upload

---
+++ Kanban-Board-App-Project/KanbanApp/UserAuthenticationService/env-variables.sh
@@ -0,0 +1,3 @@
+export MYSQL_USERNAME=root
+export MYSQL_PASSWORD=joelpc6398#
+export MYSQL_URL=jdbc:mysql://localhost:3306/users?useSSL=false&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true
