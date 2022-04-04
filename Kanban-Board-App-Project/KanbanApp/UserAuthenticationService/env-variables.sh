31c96135236fdd620f895de1c9d92c97301d4abc
Joel <91497556+jo9434@users.noreply.github.com>
2022-04-05 00:22:38 +0530

    Delete Kanban-Board-App-Project/KanbanApp/UserAuthenticationService directory

--- Kanban-Board-App-Project/KanbanApp/UserAuthenticationService/env-variables.sh
+++
@@ -1,3 +0,0 @@
-export MYSQL_USERNAME=root
-export MYSQL_PASSWORD=joelpc6398#
-export MYSQL_URL=jdbc:mysql://localhost:3306/users?useSSL=false&createDatabaseIfNotExist=true&allowPublicKeyRetrieval=true
